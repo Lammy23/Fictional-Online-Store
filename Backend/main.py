d9f8280e65dd93e7fc99f953f3ad94a0de613f19
RichardAranz <richard.arannonu@gmail.com>
2024-05-10 14:44:11 +0100

    initialised flask

--- Backend/main.py
+++ Backend/main.py
@@ -1,1 +1,8 @@
-## Flask Code goes here
+from flask import Flask, redirect, render_template, request, session, url_for, flash, sessions
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user, UserMixin
+
+
+# Initialize Flask application
+app = Flask(__name__)
+# Secret key required for sessions
+app.secret_key = 'richardarannonu07036232511chinem'
