554429141104f5cefa0a51dea0619db9eda1ae18
RichardAranz <richard.arannonu@gmail.com>
2024-05-11 19:34:26 +0100

    new mad

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,21 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///test.db'
 
-# Example route to serve as an API endpoint
-@app.route('/api/data')
-def get_data():
-    # This could be data fetched from a database or generated dynamically
-    data = {'message': 'Hello from Flask!'}
-    return jsonify(data)
 
-# Example route to serve the React frontend
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve_frontend(path):
-    # Serve the React frontend from the build directory
-    return app.send_static_file('index.html')
+
+
+@app.route('/', methods=['GET'])
+def get_articles():
+    return jsonify({{"Hello":"Worlds"}})
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
